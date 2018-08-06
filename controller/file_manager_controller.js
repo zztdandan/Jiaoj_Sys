708fb4e6b3364b831b9071165827a2fc28e4c5b0
80822215 <zztdandan@gmail.com>
2018-08-06 10:46:44 +0800

    upload测试通过

--- controller/file_manager_controller.js
+++ controller/file_manager_controller.js
@@ -25,15 +25,15 @@ router.post('/uploadpic', function (req, res, next) {
             throw err;
         }
         //第一步，获得文件地址
-        var filePath = '';
+        var tmpfilePath = '';
         var fileSavepath = '';
         //如果提交文件的form中将上传文件的input名设置为tmpFile，就从tmpFile中取上传文件。否则取for in循环第一个上传的文件。
         if (file.tmpFile) {
-            filePath = file.tmpFile.path;
+            tmpfilePath = file.tmpFile.path;
         } else {
             for (var key in file) {
-                if (file[key].path && filePath === '') {
-                    filePath = file[key].path;
+                if (file[key].path && tmpfilePath === '') {
+                    tmpfilePath = file[key].path;
                     break;
                 }
             }
@@ -41,7 +41,7 @@ router.post('/uploadpic', function (req, res, next) {
 
         //第二部,判断文件类型是否允许上传
         var fileExt1;
-        var fileExt = filePath.substring(filePath.lastIndexOf('.'));
+        var fileExt = tmpfilePath.substring(tmpfilePath.lastIndexOf('.'));
         if (('.jpg.jpeg.png.gif').indexOf(fileExt.toLowerCase()) === -1) {
             var err = new Error('此文件类型不允许上传');
             res.json({ code: -1, message: '此文件类型不允许上传' });
@@ -50,7 +50,9 @@ router.post('/uploadpic', function (req, res, next) {
         }
 
         //第三步，已经获悉文件可以上传，现在读取暂存在tmp里面的文件
-        fs.readFile(filePath, function (err, data) {
+        //tmpfilePath:暂存的文件的路径
+        fs.readFile(tmpfilePath, function (err, data) {
+            //data，已经将暂存文件读入缓存中
             if (err) {
                 err.status = 415;
 
@@ -70,16 +72,23 @@ router.post('/uploadpic', function (req, res, next) {
             file_manager.FTP_PATH = fileSavepath + '/' + filename;
             //初始化ftp函数
             var f_c = new ftp_client();
-            f_c.upLoad(data, file_manager.FTP_PATH, function (err) {
+            //上传buffer到ftp
+            f_c.upLoad(data, file_manager.FTP_PATH, function () {
+                //进入此处代表上传成功（前面已经做过错误处理）
+                console.log('upLoaded');
+            });
+            //回传rec_id到post回执
+            file_manager_model.additem(file_manager, function (id) {
+                res.json({ "id": file_manager.REC_ID  });
+            });
+            //删除临时目录中的文件
+            fs.unlink(tmpfilePath,function(err)
+            {
                 if (err) {
-                    err.status = 416;
-                    throw err;
+                    throw new Error("删除暂存文件失败");
                 }
             });
-            file_manager_model.additem(file_manager, function (id) {
-                res.json({ "id": id });
-            })
-            // console.log("异步读取: " + data.toString());
+        
         });
 
 
@@ -87,8 +96,13 @@ router.post('/uploadpic', function (req, res, next) {
     });
 
 });
+router.get('/load', function (req, res, next) {
 
-router.Download = function (req, res, next) {
+    if (typeof (req.query.id) == "undefined") {//没有指定id
+        throw new Error("没有指定id");
+    }
+    var rec_id = req.query.id;
 
-}
+});
+
 module.exports = router;
--- controller/ftp_client.js
+++ controller/ftp_client.js
@@ -4,10 +4,11 @@ var fs = require('fs');
 var concat = require('concat-stream');
 var ftp_config = require('./ftp_config');
 var ftp_client = function () {
-    var c = new client();
+
 
     //path:需要下载的文件的完整地址，next:回调函数
     this.Download = function (path, next) {
+        var c = new client();
         c.on('ready', function () {
 
             c.get(path, function (err, stream) {
@@ -25,25 +26,49 @@ var ftp_client = function () {
             });
 
         });
+        c.connect({
+            host: "127.0.0.1",
+            port: 21,
+            user: "jiaojftp",
+            password: "jiaojftp"
+        });
     }
     //buffer:图片内容缓存,path:图片存储的ftp地址(完整，包括文件名和后缀),next:回调函数
     this.upLoad = function (buffer, path, next) {
 
+
+        var c = new client();
+        var filepath = path.substring(0, path.lastIndexOf('/'));
+
         c.on('ready', function () {
+            c.list(filepath, function (err, list) {
+                if (err) {
+                    //此处参数true 递归创建目录，可创建多级
+                    c.mkdir(filepath, true, function (err) {
+                        if (err) throw err;
+                    })
+                }
 
-            c.put('a', 'foo.remote-copy.jpg', function (err) {
+                c.put(buffer, path, function (err) {
 
-                if (err) throw err;
+                    if (err) throw err;
 
-                c.end();
+                    c.end();
+                    next();
+                });
 
-            });
+            })
 
 
         });
-        c.connect(ftp_config);
+        c.connect({
+            host: "127.0.0.1",
+            port: 21,
+            user: "jiaojftp",
+            password: "jiaojftp"
+        });
     }
-    c.connect(ftp_config);
+
 
 
 }
