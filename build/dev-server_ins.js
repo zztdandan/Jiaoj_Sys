7af6bec507d516df5b43673f73f5541e4c02dfb2
80822215 <zztdandan@gmail.com>
2018-08-19 09:50:01 +0800

    express vue 踩坑基本完成，正确跳转首页

--- build/dev-server_ins.js
+++ build/dev-server_ins.js
@@ -1,20 +1,18 @@
-require('./check-versions')()
+require('./check-versions')();
 
-var config = require('../config')
+var config = require('../config');
 if (!process.env.NODE_ENV) {
-  process.env.NODE_ENV = JSON.parse(config.dev.env.NODE_ENV)
+  process.env.NODE_ENV = JSON.parse(config.dev.env.NODE_ENV);
 }
 var http = require('http');
-
-var opn = require('opn')
-var path = require('path')
-var express = require('express')
-var webpack = require('webpack')
-var proxyMiddleware = require('http-proxy-middleware')
-var webpackConfig = require('./webpack.dev.conf')
+var opn = require('opn');
+var path = require('path');
+var express = require('express');
+var webpack = require('webpack');
+// var proxyMiddleware = require('http-proxy-middleware');
+var webpackConfig = require('./webpack.dev.conf');
 var domain = require('domain');
 var createError = require('http-errors');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
@@ -23,30 +21,37 @@ var ejs = require('ejs');
 // default port where dev server listens for incoming traffic
 var port =3000;
 // automatically open browser, if not set will be false
-var autoOpenBrowser = !!config.dev.autoOpenBrowser
+var autoOpenBrowser = true;
 // Define HTTP proxies to your custom API backend
 // https://github.com/chimurai/http-proxy-middleware
-var proxyTable = config.dev.proxyTable
+var proxyTable = {};
 
-var app = express()
-var compiler = webpack(webpackConfig)
+var app = express();
+var compiler = webpack(webpackConfig);
+
+
 
 var devMiddleware = require('webpack-dev-middleware')(compiler, {
   publicPath: webpackConfig.output.publicPath,
   quiet: false
-})
+});
+
+
 
 var hotMiddleware = require('webpack-hot-middleware')(compiler, {
   log: false,
   heartbeat: 2000
-})
+});
 // force page reload when html-webpack-plugin template changes
 compiler.plugin('compilation', function (compilation) {
   compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
-    hotMiddleware.publish({ action: 'reload' })
-    cb()
-  })
-})
+    hotMiddleware.publish({ action: 'reload' });
+    cb();
+  });
+});
+
+
+
 
 // proxy api requests
 // Object.keys(proxyTable).forEach(function (context) {
@@ -61,11 +66,21 @@ compiler.plugin('compilation', function (compilation) {
 // app.use(require('connect-history-api-fallback')())
 
 // serve webpack bundle output
-app.use(devMiddleware)
+app.use(devMiddleware);
+
+
+
+
+
+
+var routes = require(path.join(process.cwd(), 'routes', 'index'));
+routes(app);
+
+
 
 // enable hot-reload and state-preserving
 // compilation error display
-app.use(hotMiddleware)
+app.use(hotMiddleware);
 
 // serve pure static assets
 // var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
@@ -76,7 +91,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(.bodyParser({uploadDir:'./tmp'}));
 app.use(cookieParser());
-app.use(express.static(path.join(process.cwd(), 'public')));
+app.use(express.static(path.join(process.cwd(), 'src')));
 
 //在express里面允许所有请求
 // 允许所有的请求形式 
@@ -87,6 +102,7 @@ app.use(function (req, res, next) {
 });
 
 //设定全局视图解析引擎
+app.set('view engine','ejs');
 //让html调用ejs引擎（等于html可以写ejs）
 app.engine('html', ejs.__express);
 //设定views目录为全局，调用views将从根目录开始写路径
@@ -95,30 +111,25 @@ app.set('views', path.join(process.cwd(), ''));
 
 
 
-var routes = require(path.join(process.cwd(), 'routes', 'index'));
-routes(app);
 
 
 
-
-
-
-var uri = 'http://localhost:' + port
+var uri = 'http://localhost:' + port;
 
 var _resolve;
 var readyPromise = new Promise(resolve => {
-  _resolve = resolve
-})
+  _resolve = resolve;
+});
 
-console.log('> Starting dev server...')
+console.log('> Starting dev server...');
 devMiddleware.waitUntilValid(() => {
-  console.log('> Listening at ' + uri + '\n')
+  console.log('> Listening at ' + uri + '\n');
   // when env is testing, don't need open it
   if (autoOpenBrowser && process.env.NODE_ENV !== 'testing') {
-    opn(uri)
+    opn(uri);
   }
-  _resolve()
-})
+  _resolve();
+});
 
 /**
  * Get port from environment and store in Express.
@@ -203,6 +214,6 @@ process.on('uncaughtException', function (err) {
 module.exports = {
   ready: readyPromise,
   close: () => {
-    server.close()
+    server.close();
   }
-}
+};
--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,10 +1,10 @@
-var path = require('path')
-var utils = require('./utils')
-var config = require('../config')
-var vueLoaderConfig = require('./vue-loader.conf')
+var path = require('path');
+var utils = require('./utils');
+var config = require('../config');
+var vueLoaderConfig = require('./vue-loader.conf');
 
 function resolve(dir) {
-  return path.join(__dirname, '..', dir)
+  return path.join(process.cwd(), dir);
 }
 
 module.exports = {
@@ -54,4 +54,4 @@ module.exports = {
     }
     ]
   }
-}
+};
