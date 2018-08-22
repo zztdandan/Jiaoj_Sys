cd7b984c0e3ad68621a123125634ff86e90e7001
80822215 <zztdandan@gmail.com>
2018-08-22 23:38:56 +0800

    针对性更改

--- menu/controller/menu_controller.js
+++ menu/controller/menu_controller.js
@@ -3,28 +3,59 @@ var router = express.Router();
 var path = require('path');
 var user_menu_model = require(path.join(process.cwd(), 'menu', 'model', 'user_menu_model'));
 const csexception = require(path.join(process.cwd(), 'logic', 'csexception'));
-
-
-
+var linqjs = require('linqjs');
 //获得menulist
-router.get('/', function (req, res, next) {
-    user_menu_model.read_user_menu(function (menuList) {
-        console.log('menu_controller');
-       
-        res.json( new csexception(true,'suc',menuList));
+router.get('/', function(req, res, next) {
+  user_menu_model.read_user_menu().then(function(menuList) {
+    console.log('menu_controller');
+
+    res.json(new csexception(true, 'suc', menuList));
+  });
+});
+//获得业务大类的ntext，在code_desc，由于有多项，所以是直接输出list的。在前台是直接读取dic+list[i].CODE_DESC
+router.get('/allmenu', function(req, res, next) {
+  async function doall() {
+    let menu_dic = await user_menu_model.read_user_menu_dic();
+    let menu_list = await user_menu_model.read_user_menu();
+    let returnObj = [];
+    var one_menu_list = function(one_cate) {
+      var show = new Array();
+      let more=new Array();
+      menu_list.forEach(x => {
+        if (x.MENU_CATE == one_cate.name) {
+            if(show.length<one_cate.top_num){
+                show.push(x);
+            }
+            else{
+                more.push(x);
+            }
+         
+        }
+      });
+
+      return {show:show,more:more};
+    };
+    menu_dic.forEach(one_cate => {
+      let detail = JSON.parse(one_cate.CODE_DESC);
+      
+        var twolist= one_menu_list(detail);
+        detail.show_menu=twolist.show;
+        detail.more_menu=twolist.more;
+        returnObj.push(detail);
+    
     });
-
-
+    res.json(new csexception(true, 'suc', returnObj));
+  }
+  doall();
 });
 
-
 //获得业务大类的ntext，在code_desc，由于有多项，所以是直接输出list的。在前台是直接读取dic+list[i].CODE_DESC
-router.get('/dic', function (req, res, next) {
-    user_menu_model.read_user_menu_dic(function (dic_list) {
-        // console.log('menu_controller');
-  
-        res.json( new csexception(true,'suc',dic_list));
-    });
+router.get('/dic', function(req, res, next) {
+  user_menu_model.read_user_menu_dic().then(function(dic_list) {
+    // console.log('menu_controller');
+
+    res.json(new csexception(true, 'suc', dic_list));
+  });
 });
 
 module.exports = router;
--- menu/model/user_menu_model.js
+++ menu/model/user_menu_model.js
@@ -2,30 +2,26 @@ var path = require('path');
 var easy_mysql = require(process.cwd() + '/mydb/easy_mysql');
 var data_dic_model = require(path.join(process.cwd(), 'model', 'data_dic_model'));
 
-
-var user_menu_model = function () {
-
-};
-user_menu_model.read_user_menu = function (next) {
+var user_menu_model = function() {};
+user_menu_model.read_user_menu = function() {
+  return new Promise((resolve, reject) => {
     var em = new easy_mysql('user_menu');
-    em.query('select * from user_menu', function (err, rows) {
-
-        if (err) {
-            throw err;
-        }
-        rows;
-        next(rows);
+    em.query('select * from user_menu', function(err, rows) {
+      if (err) {
+        reject(err) ;
+      }
+      resolve(rows);
     });
-
+  });
 };
 
 //获得业务大类的ntext，在code_desc，由于有多项，所以是直接输出list的
-user_menu_model.read_user_menu_dic = function (next) {
-    data_dic_model.find_list_by_cname('业务大类', function (list) {
-        next(list);
+user_menu_model.read_user_menu_dic = function() {
+  return new Promise(function(resolve, reject) {
+    data_dic_model.find_list_by_cname('业务大类', function(list) {
+      resolve(list);
     });
-
+  });
 };
-
 
 module.exports = user_menu_model;
